a365ffc14552745223f666277d88cda9f1283226
Ali <aatia1999@gmail.com>
2024-05-29 11:40:39 +0200

    correct merge probleme

--- hackathon-jo/src/main.tsx
+++ hackathon-jo/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Accueil from './accueil/accueil.tsx'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Accueil />
+    <App />
   </React.StrictMode>,
 )
