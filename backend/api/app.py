146da916246a6a2809cce3d18bab05651e03e91b
Ali <aatia1999@gmail.com>
2024-05-30 11:53:02 +0200

    Correct db mappiing

--- backend/api/app.py
+++ backend/api/app.py
@@ -3,14 +3,15 @@ from services.dbConnection import app, db
 
 class OlympicsMedalsCleaned(db.Model):
     __tablename__ = 'olympics_medals_cleaned'
-    disciplineTitle = db.Column(db.String(50))
-    slugGame = db.Column(db.String(50))
+    id = db.Column(db.Integer, primary_key=True)
+    discipline_title = db.Column(db.String(50))
+    slug_game = db.Column(db.String(50))
 
 # Define a simple model
 @app.route('/')
 def index():
     medals = OlympicsMedalsCleaned.query.all()
-    return {'medals': [{'disciplineTitle': medal.disciplineTitle, 'slugGame': medal.slugGame} for medal in medals]}
+    return {'medals': [{'disciplineTitle': medal.discipline_title, 'slugGame': medal.slug_game} for medal in medals]}
 
 if __name__ == '__main__':
     app.run(debug=True)
