a3abbb3ee57c8a0a7989abbb91f67fc20c52fd53
Ali <aatia1999@gmail.com>
2024-05-30 15:06:59 +0200

    change flast to fastapi

--- backend/api/dbConnection.py
+++ backend/api/dbConnection.py
@@ -1,29 +1,19 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+import databases
 from dotenv import load_dotenv
-import pymysql
 
-pymysql.install_as_MySQLdb()
-
-# Load environment variables from a .env file if it exists
 load_dotenv()
 
-def create_app():
-    app = Flask(__name__)
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_NAME = os.getenv('DB_NAME')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT')
 
-    # Use PyMySQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_POOL_SIZE'] = 20
+DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-    db = SQLAlchemy(app)
-    return app, db
+database = databases.Database(DATABASE_URL)
 
-app, db = create_app()
+def get_database():
+    return database
