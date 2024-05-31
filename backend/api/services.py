6e31ca1d9039eafb703b8677e7e63fddbbdcd0b3
Ali <aatia1999@gmail.com>
2024-05-31 13:23:12 +0200

    api.model for import

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,8 +1,8 @@
 
-from model.olympic_athletes import Athlete
-from model.olympic_hosts import Host
-from model.olympic_medals import Medal
-from model.olympic_results import Result
+from api.model.olympic_athletes import Athlete
+from api.model.olympic_hosts import Host
+from api.model.olympic_medals import Medal
+from api.model.olympic_results import Result
 from api.dbConnection import get_database
 
 db = get_database()
